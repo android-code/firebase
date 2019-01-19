8e6ff6207c53aad27a92d3dffb0da81a217ed032
Maciej Nowak <kontakt@maciej-nowak.pl>
2019-01-19 16:01:02 +0100

    crashlytics code added

---
+++ crashlytics/MainActivity.kt
@@ -0,0 +1,56 @@
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonDivide.setOnClickListener {
+            try {
+                //mock exception
+                val value = div(10, 0)
+                //do more work
+            } 
+            catch (e: Exception) {
+                Crashlytics.logException(e)
+            }
+        }
+
+        buttonThirdValue.setOnClickListener {
+            val values = arrayListOf(1,2) //mock values
+            try {
+                //mock exception
+                val value = getThirdValue(values)
+                //do more work
+            } 
+            catch (e: Exception) {
+                Crashlytics.log("buttonThirdValue action error extra log") //to logcat
+                Crashlytics.setInt("first_number", values[0])
+                Crashlytics.logException(e) //log to Firebase
+            }
+        }
+
+        //Crashlytics.getInstance().crash(); //run it to force crash
+    }
+
+    private fun div(a: Int, b: Int): Int {
+        return a/b
+    }
+
+    private fun getThirdValue(values: ArrayList<Int>): Int {
+        return values[2]
+    }
+
+    //AUTOREPORTING--------------------------
+    //run to enable autoreporting if disabled
+    private fun enableCrashlytics() {
+        val enable = true //mock value, get from some preferences
+        if(enable)
+            Fabric.with(this, Crashlytics())
+    }
+
+    /*add to AndroidManifest to disable autoreporting
+    <meta-data
+        android:name="firebase_crashlytics_collection_enabled"
+        android:value="false" />
+    */
+}
---
+++ crashlytics/README.md
@@ -0,0 +1,2 @@
+# crashlytics
+This is repository of http://androidcode.pl blog. It shows uses Crashlytics in Android. It is a part of Firebase - Crashlytics post in the blog.
